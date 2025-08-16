6314d897db6d714849ab9772c16436cf19955059
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-16 06:23:46 +0000

    Aug 16, 2025, 10:23 AM

--- Riselab/app/static/blogs.js
+++ Riselab/app/static/blogs.js
@@ -1,5 +1,5 @@
-const burger = document.querySelector('.rise-burger');
-const navLinks = document.querySelector('.rise-nav-links');
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('open');

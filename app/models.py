adeb5b4528e174ea9dcb902b33a3b8de056dcecf
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-19 05:39:02 +0000

    Aug 19, 2025, 9:39 AM

--- app/models.py
+++ app/models.py
@@ -19,6 +19,7 @@ class Project(models.Model):
     description_eng = models.TextField()
     short_description_eng = models.TextField()
     uploaded_at = models.DateField()
+    image = models.ImageField(upload_to='projects_images/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.title}"
